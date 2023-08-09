28ca9439c30d0a62696a22c0a1337969cc5460da
Kin Tuda <kintuda17@gmail.com>
2023-08-08 22:04:49 -0300

    feat: remove go code

--- pkg/config/app.go
+++
@@ -1,7 +0,0 @@
-package config
-
-type AppConfig struct {
-	Env         string `env:"ENV"`
-	HttpPort    string `env:"HTTP_PORT"`
-	PostgresDns string `env:"POSTGRES_DNS"`
-}
--- pkg/converter/service.go
+++
@@ -1,7 +0,0 @@
-package converter
-
-import "context"
-
-type HtmlConversionProvider interface {
-	SendSMS(ctx context.Context, content string) (string, error)
-}
